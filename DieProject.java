6e35606c45f412ad82ec78d48d41b533d6332592
DoranLoe <doranloescher@gmail.com>
2026-04-17 17:04:19 -0700

    Create DieProject.java

---
+++ DieProject.java
@@ -0,0 +1,11 @@
+/** 
+* A java project for analuzing different dice combinations, showing their min, max, average in mean median and mode, standard deviation and maybe show a graph like in AnyDice
+*/
+public class DieProject
+{
+    void main()
+    {
+
+    }
+
+}
