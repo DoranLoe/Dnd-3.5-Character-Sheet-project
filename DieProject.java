f54969b519acf6fbe4ee34efa1cd708c923635d4
DoranLoe <doranloescher@gmail.com>
2026-04-23 15:22:31 -0700

    uploading unfinished uml file for new project idea
    I don't really want to do the dice thing it feels too small and uninteresting when I could just use Anydice so I'm pivoting.

--- DieProject.java
+++ DieProject.java
@@ -1,11 +1,8 @@
 /** 
-* A java project for analuzing different dice combinations, showing their min, max, average in mean median and mode, standard deviation and maybe show a graph like in AnyDice
+* A java project for analizing different dice combinations, showing their min, max, average in mean median and mode, standard deviation and maybe show a graph like in AnyDice
 */
 public class DieProject
 {
     void main()
-    {
-
-    }
-
+    {}
 }
