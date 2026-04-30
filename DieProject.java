0fca1374b0544dd7c9955f7c772ab0169984babe
DoranLoe <doranloescher@gmail.com>
2026-04-30 15:58:39 -0700

    Delete DieProject.java

--- DieProject.java
+++
@@ -1,8 +0,0 @@
-/** 
-* A java project for analizing different dice combinations, showing their min, max, average in mean median and mode, standard deviation and maybe show a graph like in AnyDice
-*/
-public class DieProject
-{
-    void main()
-    {}
-}
